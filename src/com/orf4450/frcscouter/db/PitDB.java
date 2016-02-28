9d2a8ac73ba2ed8032911c56884662abc71bd59d
ShortCircuit908 <P_Ir_At_Es@comcast.net>
2016-02-27 16:30:12 -0800

    Fixed crashes with DB

--- src/com/orf4450/frcscouter/db/PitDB.java
+++ src/com/orf4450/frcscouter/db/PitDB.java
@@ -49,82 +49,94 @@ public class PitDB extends ScouterDB {
 
 	}
 
-	public void save() {
-		TextViewColumnBinding team_number = column_binder.get(R.id.pit_team_number);
-		TextViewColumnBinding team_name = column_binder.get(R.id.pit_team_name);
-		boolean validated = true;
-		if (team_number.getValue().toString().trim().isEmpty()) {
-			team_number.setError("This field is required");
-			validated = false;
+	public boolean save() {
+		if(column_binder != null) {
+			TextViewColumnBinding team_number = column_binder.get(R.id.pit_team_number);
+			TextViewColumnBinding team_name = column_binder.get(R.id.pit_team_name);
+			boolean validated = true;
+			if (team_number.getValue().toString().trim().isEmpty()) {
+				team_number.setError("This field is required");
+				validated = false;
+			}
+			else {
+				team_number.setError(null);
+			}
+			if (team_name.getValue().toString().trim().isEmpty()) {
+				team_name.setError("This field is required");
+				validated = false;
+			}
+			else {
+				team_name.setError(null);
+			}
+			if (!validated) {
+				return false;
+			}
+			SQLiteDatabase db = getWritableDatabase();
+			db.execSQL("DELETE FROM `" + SCOUTING_TABLE_NAME + "` WHERE `team_number`=?", new Object[]{Integer.parseInt(team_number.getValue() + "")});
+			column_binder.save(db, SCOUTING_TABLE_NAME);
+			return true;
 		}
-		else {
-			team_number.setError(null);
-		}
-		if (team_name.getValue().toString().trim().isEmpty()) {
-			team_name.setError("This field is required");
-			validated = false;
-		}
-		else {
-			team_name.setError(null);
-		}
-		if (!validated) {
-			return;
-		}
-		SQLiteDatabase db = getWritableDatabase();
-		db.execSQL("DELETE FROM `" + SCOUTING_TABLE_NAME + "` WHERE `team_number`=?", new Object[]{Integer.parseInt(team_number.getValue() + "")});
-		column_binder.save(db, SCOUTING_TABLE_NAME);
+		return false;
 	}
 
 	public void deleteAllData() {
-		getWritableDatabase().execSQL("DELETE FROM `" + SCOUTING_TABLE_NAME + "`");
-		File storage_dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-		for (File file : storage_dir.listFiles()) {
-			if (file.exists() && file.getName().matches("ROBOT_\\d+.jpg\n")) {
-				file.delete();
+		if(column_binder != null) {
+			getWritableDatabase().execSQL("DELETE FROM `" + SCOUTING_TABLE_NAME + "`");
+			File storage_dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+			for (File file : storage_dir.listFiles()) {
+				if (file.exists() && file.getName().matches("ROBOT_\\d+.jpg\n")) {
+					file.delete();
+				}
 			}
 		}
 	}
 
 	@Override
 	public void upload(OutputStream out) throws IOException {
-		Nugget<?> nugget = toNugget();
-		Nugget.writeNugget(nugget, new DataOutputStream(out));
-		getWritableDatabase().execSQL("UPDATE `" + SCOUTING_TABLE_NAME + "` SET `uploaded`=1");
+		if(column_binder != null) {
+			Nugget<?> nugget = toNugget();
+			Nugget.writeNugget(nugget, new DataOutputStream(out));
+			getWritableDatabase().execSQL("UPDATE `" + SCOUTING_TABLE_NAME + "` SET `uploaded`=1");
+		}
 	}
 
 	@Override
 	public Nugget<?> toNugget() {
 		NuggetCompound[] compounds = new NuggetCompound[0];
-		try {
-			SQLiteDatabase db = getReadableDatabase();
-			Cursor cursor = db.rawQuery("SELECT * FROM `" + SCOUTING_TABLE_NAME + "` WHERE `uploaded`=0", null);
-			compounds = new NuggetCompound[cursor.getCount()];
-			if (cursor.getCount() > 0) {
-				int i = 0;
-				while (cursor.moveToNext()) {
-					int team_number = cursor.getInt(1);
-					NuggetCompound compound = new NuggetCompound();
-					compound.addNugget(new NuggetInteger("team_number", team_number));
-					compound.addNugget(new NuggetString("team_name", cursor.getString(2)));
-					compound.addNugget(new NuggetString("robot_description", cursor.getString(3)));
+		if(column_binder != null) {
+			try {
+				SQLiteDatabase db = getReadableDatabase();
+				Cursor cursor = db.rawQuery("SELECT * FROM `" + SCOUTING_TABLE_NAME + "` WHERE `uploaded`=0", null);
+				compounds = new NuggetCompound[cursor.getCount()];
+				if (cursor.getCount() > 0) {
+					int i = 0;
+					while (cursor.moveToNext()) {
+						int team_number = cursor.getInt(1);
+						NuggetCompound compound = new NuggetCompound();
+						compound.addNugget(new NuggetInteger("team_number", team_number));
+						compound.addNugget(new NuggetString("team_name", cursor.getString(2)));
+						compound.addNugget(new NuggetString("robot_description", cursor.getString(3)));
 					/*
 					File image_file = PitScouting.getImageFile(team_number);
 					if(image_file.exists()){
 						compound.addNugget(new NuggetFile("image", image_file));
 					}
 					*/
-					compounds[i++] = compound;
+						compounds[i++] = compound;
+					}
 				}
+				cursor.close();
 			}
-			cursor.close();
-		}
-		catch (SQLiteException e){
-			// Do nothing;
+			catch (SQLiteException e) {
+				// Do nothing;
+			}
 		}
 		return new NuggetArray<>(SCOUTING_TABLE_NAME, compounds);
 	}
 
 	public void resetUploaded() {
-		getWritableDatabase().execSQL("UPDATE `" + SCOUTING_TABLE_NAME + "` SET `uploaded`=0");
+		if(column_binder != null) {
+			getWritableDatabase().execSQL("UPDATE `" + SCOUTING_TABLE_NAME + "` SET `uploaded`=0");
+		}
 	}
 }
--- src/com/orf4450/frcscouter/db/StandDB.java
+++ src/com/orf4450/frcscouter/db/StandDB.java
@@ -43,41 +43,45 @@ public class StandDB extends ScouterDB {
 		}
 	}
 
-	public void saveMatch() {
-		TextViewColumnBinding match_number = column_binder.get(R.id.match_number);
-		TextViewColumnBinding team_number = column_binder.get(R.id.team_number);
-		TextViewColumnBinding team_name = column_binder.get(R.id.team_name);
-		boolean validated = true;
-		if (match_number.getValue().toString().trim().isEmpty()) {
-			match_number.setError("This field is required");
-			validated = false;
+	public boolean saveMatch() {
+		if(column_binder != null) {
+			TextViewColumnBinding match_number = column_binder.get(R.id.match_number);
+			TextViewColumnBinding team_number = column_binder.get(R.id.team_number);
+			TextViewColumnBinding team_name = column_binder.get(R.id.team_name);
+			boolean validated = true;
+			if (match_number.getValue().toString().trim().isEmpty()) {
+				match_number.setError("This field is required");
+				validated = false;
+			}
+			else {
+				match_number.setError(null);
+			}
+			if (team_name.getValue().toString().trim().isEmpty()) {
+				team_name.setError("This field is required");
+				validated = false;
+			}
+			else {
+				team_name.setError(null);
+			}
+			if (team_number.getValue().toString().trim().isEmpty()) {
+				team_number.setError("This field is required");
+				validated = false;
+			}
+			else {
+				team_number.setError(null);
+			}
+			if (!validated) {
+				return false;
+			}
+			MatchDescriptor descriptor = new MatchDescriptor(
+					Integer.parseInt(match_number.getValue() + ""),
+					Integer.parseInt(team_number.getValue() + ""));
+			deleteMatch(descriptor);
+			SQLiteDatabase db = getWritableDatabase();
+			column_binder.save(db, SCOUTING_TABLE_NAME);
+			return true;
 		}
-		else {
-			match_number.setError(null);
-		}
-		if (team_name.getValue().toString().trim().isEmpty()) {
-			team_name.setError("This field is required");
-			validated = false;
-		}
-		else {
-			team_name.setError(null);
-		}
-		if (team_number.getValue().toString().trim().isEmpty()) {
-			team_number.setError("This field is required");
-			validated = false;
-		}
-		else {
-			team_number.setError(null);
-		}
-		if (!validated) {
-			return;
-		}
-		MatchDescriptor descriptor = new MatchDescriptor(
-				Integer.parseInt(match_number.getValue() + ""),
-				Integer.parseInt(team_number.getValue() + ""));
-		deleteMatch(descriptor);
-		SQLiteDatabase db = getWritableDatabase();
-		column_binder.save(db, SCOUTING_TABLE_NAME);
+		return false;
 	}
 
 	@Override
@@ -91,125 +95,127 @@ public class StandDB extends ScouterDB {
 	}
 
 	public void loadMatch(MatchDescriptor descriptor) {
-		HashMap<String, Object> search_params = new HashMap<>(2);
-		search_params.put(column_binder.get(R.id.match_number).getColumnName(), descriptor.getMatchNumber());
-		search_params.put(column_binder.get(R.id.team_number).getColumnName(), descriptor.getTeamNumber());
-		int id = column_binder.queryRowsMatchingParameters(getReadableDatabase(), SCOUTING_TABLE_NAME, search_params)[0];
-		column_binder.load(getReadableDatabase(), SCOUTING_TABLE_NAME, id);
+		if(column_binder != null) {
+			HashMap<String, Object> search_params = new HashMap<>(2);
+			search_params.put(column_binder.get(R.id.match_number).getColumnName(), descriptor.getMatchNumber());
+			search_params.put(column_binder.get(R.id.team_number).getColumnName(), descriptor.getTeamNumber());
+			int id = column_binder.queryRowsMatchingParameters(getReadableDatabase(), SCOUTING_TABLE_NAME, search_params)[0];
+			column_binder.load(getReadableDatabase(), SCOUTING_TABLE_NAME, id);
+		}
 	}
 
 	public void deleteAllData() {
-		SQLiteDatabase db = getWritableDatabase();
-		db.execSQL("DELETE FROM `" + SCOUTING_TABLE_NAME + "`");
+		if(column_binder != null) {
+			SQLiteDatabase db = getWritableDatabase();
+			db.execSQL("DELETE FROM `" + SCOUTING_TABLE_NAME + "`");
+		}
 	}
 
 	public ArrayList<MatchDescriptor> getAllStoredMatches() {
-		String match_number_column = column_binder.get(R.id.match_number).getColumnName();
-		StringBuilder query_builder = new StringBuilder("SELECT `").append(match_number_column).append("`, `")
-				.append(column_binder.get(R.id.team_number).getColumnName()).append("` FROM `").append(SCOUTING_TABLE_NAME)
-				.append("` ORDER BY `").append(match_number_column).append("` ASC");
-		SQLiteDatabase db = getReadableDatabase();
-		Cursor cursor = db.rawQuery(query_builder.toString(), null);
-		ArrayList<MatchDescriptor> descriptors = new ArrayList<>(cursor.getCount());
-		while (cursor.moveToNext()) {
-			descriptors.add(new MatchDescriptor(cursor.getInt(0), cursor.getInt(1)));
+		ArrayList<MatchDescriptor> descriptors = new ArrayList<>(0);
+		if(column_binder != null) {
+			String match_number_column = column_binder.get(R.id.match_number).getColumnName();
+			StringBuilder query_builder = new StringBuilder("SELECT `").append(match_number_column).append("`, `")
+					.append(column_binder.get(R.id.team_number).getColumnName()).append("` FROM `").append(SCOUTING_TABLE_NAME)
+					.append("` ORDER BY `").append(match_number_column).append("` ASC");
+			SQLiteDatabase db = getReadableDatabase();
+			Cursor cursor = db.rawQuery(query_builder.toString(), null);
+			descriptors = new ArrayList<>(cursor.getCount());
+			while (cursor.moveToNext()) {
+				descriptors.add(new MatchDescriptor(cursor.getInt(0), cursor.getInt(1)));
+			}
+			cursor.close();
 		}
-		cursor.close();
-		return descriptors;
-	}
-
-	public ArrayList<MatchDescriptor> getAllStoredMatches(boolean uploaded) {
-		String match_number_column = column_binder.get(R.id.match_number).getColumnName();
-		StringBuilder query_builder = new StringBuilder("SELECT `").append(match_number_column).append("`, `")
-				.append(column_binder.get(R.id.team_number).getColumnName()).append("` FROM `").append(SCOUTING_TABLE_NAME)
-				.append("WHERE `uploaded`=").append(uploaded ? "1" : 0).append("` ORDER BY `")
-				.append(match_number_column).append("` ASC");
-		SQLiteDatabase db = getReadableDatabase();
-		Cursor cursor = db.rawQuery(query_builder.toString(), null);
-		ArrayList<MatchDescriptor> descriptors = new ArrayList<>(cursor.getCount());
-		while (cursor.moveToNext()) {
-			descriptors.add(new MatchDescriptor(cursor.getInt(0), cursor.getInt(1)));
-		}
-		cursor.close();
 		return descriptors;
 	}
 
 	public int getLastMatchNumber() {
-		String match_number_column = column_binder.get(R.id.match_number).getColumnName();
-		SQLiteDatabase db = getReadableDatabase();
-		Cursor cursor = db.rawQuery("SELECT `" + match_number_column + "` FROM `" + SCOUTING_TABLE_NAME
-				+ "` ORDER BY `" + match_number_column + "` DESC LIMIT 1", null);
-		int last = 0;
-		if (cursor.moveToNext()) {
-			last = cursor.getInt(0);
+		if(column_binder != null) {
+			String match_number_column = column_binder.get(R.id.match_number).getColumnName();
+			SQLiteDatabase db = getReadableDatabase();
+			Cursor cursor = db.rawQuery("SELECT `" + match_number_column + "` FROM `" + SCOUTING_TABLE_NAME
+					+ "` ORDER BY `" + match_number_column + "` DESC LIMIT 1", null);
+			int last = 0;
+			if (cursor.moveToNext()) {
+				last = cursor.getInt(0);
+			}
+			cursor.close();
+			return last;
 		}
-		cursor.close();
-		return last;
+		return 0;
 	}
 
 	public void upload(OutputStream out) throws IOException {
-		Nugget<?> nugget = toNugget();
-		Nugget.writeNugget(nugget, new DataOutputStream(out));
-		getWritableDatabase().execSQL("UPDATE `" + SCOUTING_TABLE_NAME + "` SET `uploaded`=1");
+		if(column_binder != null) {
+			Nugget<?> nugget = toNugget();
+			Nugget.writeNugget(nugget, new DataOutputStream(out));
+			getWritableDatabase().execSQL("UPDATE `" + SCOUTING_TABLE_NAME + "` SET `uploaded`=1");
+		}
 	}
 
 	@Override
 	public Nugget<?> toNugget() {
 		NuggetCompound[] compounds = new NuggetCompound[0];
-		try {
-			StringBuilder query_builder = new StringBuilder("SELECT * FROM `").append(SCOUTING_TABLE_NAME)
-					.append("` WHERE `uploaded`=0");
-			SQLiteDatabase db = getReadableDatabase();
-			Cursor cursor = db.rawQuery(query_builder.toString(), null);
-			compounds = new NuggetCompound[cursor.getCount()];
-			if (cursor.getCount() > 0) {
-				int i = 0;
-				while (cursor.moveToNext()) {
-					NuggetCompound compound = new NuggetCompound();
-					for (int j = 1; j < cursor.getColumnCount(); j++) {
-						String name = cursor.getColumnName(j);
-						switch (cursor.getType(j)) {
-							case Cursor.FIELD_TYPE_FLOAT:
-								compound.addNugget(new NuggetDouble(name, cursor.getDouble(j)));
-								continue;
-							case Cursor.FIELD_TYPE_INTEGER:
-								compound.addNugget(new NuggetLong(name, cursor.getLong(j)));
-								continue;
-							case Cursor.FIELD_TYPE_NULL:
-								compound.addNugget(new NuggetString(name));
-								continue;
-							case Cursor.FIELD_TYPE_STRING:
-								compound.addNugget(new NuggetString(name, cursor.getString(j)));
-								continue;
-							case Cursor.FIELD_TYPE_BLOB:
-								compound.addNugget(NuggetFactory.wrapNuggetArray(cursor.getBlob(j)));
+		if (column_binder != null) {
+			try {
+				StringBuilder query_builder = new StringBuilder("SELECT * FROM `").append(SCOUTING_TABLE_NAME)
+						.append("` WHERE `uploaded`=0");
+				SQLiteDatabase db = getReadableDatabase();
+				Cursor cursor = db.rawQuery(query_builder.toString(), null);
+				compounds = new NuggetCompound[cursor.getCount()];
+				if (cursor.getCount() > 0) {
+					int i = 0;
+					while (cursor.moveToNext()) {
+						NuggetCompound compound = new NuggetCompound();
+						for (int j = 1; j < cursor.getColumnCount(); j++) {
+							String name = cursor.getColumnName(j);
+							switch (cursor.getType(j)) {
+								case Cursor.FIELD_TYPE_FLOAT:
+									compound.addNugget(new NuggetDouble(name, cursor.getDouble(j)));
+									continue;
+								case Cursor.FIELD_TYPE_INTEGER:
+									compound.addNugget(new NuggetLong(name, cursor.getLong(j)));
+									continue;
+								case Cursor.FIELD_TYPE_NULL:
+									compound.addNugget(new NuggetString(name));
+									continue;
+								case Cursor.FIELD_TYPE_STRING:
+									compound.addNugget(new NuggetString(name, cursor.getString(j)));
+									continue;
+								case Cursor.FIELD_TYPE_BLOB:
+									compound.addNugget(NuggetFactory.wrapNuggetArray(cursor.getBlob(j)));
+							}
 						}
+						compounds[i++] = compound;
 					}
-					compounds[i++] = compound;
 				}
+				cursor.close();
 			}
-			cursor.close();
-		}
-		catch (SQLiteException e) {
-			// Do nothing;
+			catch (SQLiteException e) {
+				// Do nothing;
+			}
 		}
 		return new NuggetArray<>(SCOUTING_TABLE_NAME, compounds);
 	}
 
 	public void deleteMatch(MatchDescriptor descriptor) {
-		String match_number = column_binder.get(R.id.match_number).getColumnName();
-		String team_number = column_binder.get(R.id.team_number).getColumnName();
-		Cursor cursor = getReadableDatabase().rawQuery("SELECT `_id` FROM `" + SCOUTING_TABLE_NAME + "` WHERE `"
-				+ match_number + "`=" + descriptor.getMatchNumber() + " AND `" + team_number
-				+ "`=" + descriptor.getTeamNumber(), null);
-		if (!cursor.moveToNext()) {
-			return;
+		if (column_binder != null) {
+			String match_number = column_binder.get(R.id.match_number).getColumnName();
+			String team_number = column_binder.get(R.id.team_number).getColumnName();
+			Cursor cursor = getReadableDatabase().rawQuery("SELECT `_id` FROM `" + SCOUTING_TABLE_NAME + "` WHERE `"
+					+ match_number + "`=" + descriptor.getMatchNumber() + " AND `" + team_number
+					+ "`=" + descriptor.getTeamNumber(), null);
+			if (!cursor.moveToNext()) {
+				return;
+			}
+			int id = cursor.getInt(0);
+			getWritableDatabase().execSQL("DELETE FROM `" + SCOUTING_TABLE_NAME + "` WHERE `_id`=?", new Object[]{id});
 		}
-		int id = cursor.getInt(0);
-		getWritableDatabase().execSQL("DELETE FROM `" + SCOUTING_TABLE_NAME + "` WHERE `_id`=?", new Object[]{id});
 	}
 
 	public void resetUploaded() {
-		getWritableDatabase().execSQL("UPDATE `" + SCOUTING_TABLE_NAME + "` SET `uploaded`=0");
+		if (column_binder != null) {
+			getWritableDatabase().execSQL("UPDATE `" + SCOUTING_TABLE_NAME + "` SET `uploaded`=0");
+		}
 	}
 }
