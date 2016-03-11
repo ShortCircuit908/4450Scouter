f6e7b72e9953b2ef2a8fba075f17388e0b714729
ShortCircuit908 <P_Ir_At_Es@comcast.net>
2016-03-11 09:42:10 -0800

    Change boolean column length to 1, add default name bindings

--- src/com/orf4450/frcscouter/stand/ScouterConstants.java
+++ src/com/orf4450/frcscouter/stand/ScouterConstants.java
@@ -10,52 +10,62 @@ public interface ScouterConstants {
 	int[][] pickup_speed_bindings = {
 			{R.id.auto_pickup_fast, 0},
 			{R.id.auto_pickup_medium, 1},
-			{R.id.auto_pickup_slow, 2}
+			{R.id.auto_pickup_slow, 2},
+			{-1, 3}
 	};
 	int[][] portcullis_speed_bindings = {
 			{R.id.portcullis_fast, 0},
 			{R.id.portcullis_medium, 1},
-			{R.id.portcullis_slow, 2}
+			{R.id.portcullis_slow, 2},
+			{-1, 3}
 	};
 	int[][] chival_speed_bindings = {
 			{R.id.chival_fast, 0},
 			{R.id.chival_medium, 1},
-			{R.id.chival_slow, 2}
+			{R.id.chival_slow, 2},
+			{-1, 3}
 	};
 	int[][] moat_speed_bindings = {
 			{R.id.moat_fast, 0},
 			{R.id.moat_medium, 1},
-			{R.id.moat_slow, 2}
+			{R.id.moat_slow, 2},
+			{-1, 3}
 	};
 	int[][] ramparts_speed_bindings = {
 			{R.id.ramparts_fast, 0},
 			{R.id.ramparts_medium, 1},
-			{R.id.ramparts_slow, 2}
+			{R.id.ramparts_slow, 2},
+			{-1, 3}
 	};
 	int[][] drawbridge_speed_bindings = {
 			{R.id.drawbridge_fast, 0},
 			{R.id.drawbridge_medium, 1},
-			{R.id.drawbridge_slow, 2}
+			{R.id.drawbridge_slow, 2},
+			{-1, 3}
 	};
 	int[][] sally_speed_bindings = {
 			{R.id.sally_fast, 0},
 			{R.id.sally_medium, 1},
-			{R.id.sally_slow, 2}
+			{R.id.sally_slow, 2},
+			{-1, 3}
 	};
 	int[][] rock_speed_bindings = {
 			{R.id.rock_fast, 0},
 			{R.id.rock_medium, 1},
-			{R.id.rock_slow, 2}
+			{R.id.rock_slow, 2},
+			{-1, 3}
 	};
 	int[][] rough_speed_bindings = {
 			{R.id.rough_fast, 0},
 			{R.id.rough_medium, 1},
-			{R.id.rough_slow, 2}
+			{R.id.rough_slow, 2},
+			{-1, 3}
 	};
 	int[][] low_speed_bindings = {
 			{R.id.low_fast, 0},
 			{R.id.low_medium, 1},
-			{R.id.low_slow, 2}
+			{R.id.low_slow, 2},
+			{-1, 3}
 	};
 	int[][] endgame_bindings = {
 			{R.id.on_ramp, 0},
